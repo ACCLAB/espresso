a5c31228ce76af9b314557a86f3be06bcab19e6c
Joses W. Ho <joseshowh@gmail.com>
2017-10-05 16:19:40 +0800

    refactored to import libs within funcs

--- _plotter/contrast.py
+++ _plotter/contrast.py
@@ -7,29 +7,6 @@
 contrast plot functions for espresso objects.
 """
 
- #      # #####  #####    ##   #####  #   #    # #    # #####   ####  #####  #####
- #      # #    # #    #  #  #  #    #  # #     # ##  ## #    # #    # #    #   #
- #      # #####  #    # #    # #    #   #      # # ## # #    # #    # #    #   #
- #      # #    # #####  ###### #####    #      # #    # #####  #    # #####    #
- #      # #    # #   #  #    # #   #    #      # #    # #      #    # #   #    #
- ###### # #####  #    # #    # #    #   #      # #    # #       ####  #    #   #
-
-import sys as _sys
-import os as _os
-
-import numpy as _np
-import scipy as _sp
-import pandas as _pd
-
-import matplotlib as _mpl
-import matplotlib.pyplot as _plt
-import matplotlib.ticker as _tk
-
-import seaborn as _sns
-import bootstrap_contrast as _bsc
-
-from . import plot_helpers as _pth
-
 class contrast_plotter:
     """
     contrast plotting class for espresso object.
@@ -42,13 +19,59 @@ class contrast_plotter:
     #    #   ##    #      #
     #    #    #    #      #
 
-    def __init__(self,plotter): # pass along an espresso_plotter instance.
-        self._experiment=plotter._experiment
+    def __init__(self,plotter):
+        self.__feeds=plotter._experiment.feeds.copy()
 
-    def __generic_contrast_plotter():
-        pass
+    def __generic_contrast_plotter(self,
+                                   df,
+                                   yvar,
+                                   color_by=None,
+                                   fig_size=None,
+                                   ax=None,
+                                   contrastplot_kwargs=None):
 
-    def feed_count_per_fly(self, group_by, palette_type='categorical',contrastplot_kwargs=None):
+        import numpy as __np
+        import pandas as __pd
+
+        import bootstrap_contrast as __bsc
+
+        from . import plot_helpers as __pth
+        from _munger import munger as __munger
+
+        # Handle the yvar and color_by keywords.
+        __munger.check_column(yvar,df)
+        if color_by is not None:
+            __munger.check_column(color_by,df)
+
+        # Handle contrastplot keyword arguments.
+        default_kwargs=dict(fig_size=(12,9),
+                            float_contrast=False,
+                            font_scale=1.4,
+                            swarmplot_kwargs={'size':6,
+                                              'hue':color_by})
+        if contrastplot_kwargs is None:
+            contrastplot_kwargs=default_kwargs
+        else:
+            contrastplot_kwargs=__bsc.merge_two_dicts(default_kwargs,contrastplot_kwargs)
+
+        # Select palette.
+        if palette_type=='categorical':
+            color_palette=__pth._make_categorial_palette(df,group_by)
+        elif palette_type=='sequential':
+            color_palette=__pth._make_sequential_palette(df,group_by)
+
+        f,b=__bsc.contrastplot(data=df,
+                              x=group_by,y=yvar,
+                              idx=__np.sort(total_feeds.loc[:,group_by].unique()),
+                              **contrastplot_kwargs)
+        return f,b
+
+    def feed_count_per_fly(self,
+                           color_by=None,
+                           fig_size=None,
+                           ax=None,
+                           palette_type='categorical',
+                           contrastplot_kwargs=None):
 
         """
         Produces a contrast plot depicting the mean differences in the feed counts between groups.
@@ -56,8 +79,13 @@ class contrast_plotter:
 
         Keywords
         --------
-        group_by: string, categorical
-            The column or label indicating the categorical grouping on the x-axis.
+        group_by: string, default None
+            Accepts a categorical column in the espresso object. Each group in this column
+            will be plotted on its own axes.
+
+        color_by: string, default None
+            Accepts a categorical column in the espresso object. Each group in this column
+            will be colored seperately, and stacked as an area plot.
 
         palette_type: string, 'categorical' or 'sequential'.
 
@@ -67,35 +95,23 @@ class contrast_plotter:
         Returns
         -------
 
-        A matplotlib Axes instance, and a pandas DataFrame with the statistics.
+        A matplotlib Figure, and a pandas DataFrame with the statistics.
         """
+        import numpy as __np
+        import pandas as __pd
 
-        default_kwargs=dict(fig_size=(12,9),
-                            float_contrast=False,
-                            font_scale=1.4,
-                            swarmplot_kwargs={'size':6})
-        if contrastplot_kwargs is None:
-            contrastplot_kwargs=default_kwargs
-        else:
-            contrastplot_kwargs=_bsc.merge_two_dicts(default_kwargs,contrastplot_kwargs)
-
-        # Select palette.
-        if palette_type=='categorical':
-            color_palette=_pth._make_categorial_palette(self._experiment.feeds,group_by)
-        elif palette_type=='sequential':
-            color_palette=_pth._make_sequential_palette(self._experiment.feeds,group_by)
-
-        total_feeds=_pd.DataFrame(self._experiment.feeds[['FlyID',group_by,'FeedVol_nl']].\
+        total_feeds=__pd.DataFrame(self.__feeds[['FlyID',group_by,'FeedVol_nl']].\
                                     groupby([group_by,'FlyID']).count().\
                                     to_records())
         total_feeds.columns=[group_by,'FlyID','Total feed count\nper fly']
-        max_feeds=_np.round(total_feeds.max()['Total feed count\nper fly'],decimals=-2)
+        max_feeds=__np.round(total_feeds.max()['Total feed count\nper fly'],decimals=-2)
 
-        f,b=_bsc.contrastplot(data=total_feeds,
-                              x=group_by,y='Total feed count\nper fly',
-                              idx=_np.sort(total_feeds.loc[:,group_by].unique()),
-                              **contrastplot_kwargs)
-
+        f, b=__generic_contrast_plotter(yvar='Total feed count\nper fly',
+                                        color_by=color_by,
+                                        fig_size=fig_size,
+                                        ax=ax,
+                                        palette_type='categorical',
+                                        contrastplot_kwargs=contrastplot_kwargs)
         f.suptitle('Contrast Plot Total Feed Count Per Fly')
 
-        return f,b
+        return f, b
