609b835cc39026ef2ccc72675d0e1fec791b9837
Joses W. Ho <joseshowh@gmail.com>
2017-12-11 10:26:53 +0800

    update pandas dep version

--- setup.py
+++ setup.py
@@ -34,9 +34,9 @@ def check_dependencies():
     try:
         import pandas
         if int(pandas.__version__.split('.')[1])<20:
-            to_install.append('pandas>=0.20.1')
+            to_install.append('pandas>=0.21')
     except ImportError:
-        to_install.append('pandas>=0.20.1')
+        to_install.append('pandas>=0.21')
     try:
         import seaborn
     except ImportError:
