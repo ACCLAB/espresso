574e427d048d561f87d19615a5edd516676101f7
Joses W. Ho <joseshowh@gmail.com>
2018-03-01 16:17:09 +0800

    working version of faceting for raster plot

--- espresso/_plotter/espresso_plotter.py
+++ espresso/_plotter/espresso_plotter.py
@@ -48,74 +48,69 @@ class espresso_plotter():
         self.timecourse = timecourse.timecourse_plotter(self)
         self.cumulative = cumulative.cumulative_plotter(self)
 
-    def __plot_rasters(current_facet_feeds, current_facet_flies,color_by,
+    def __plot_rasters(self, current_facet_feeds, current_facet_flies,
+                       maxflycount, color_by, palette,
                        plot_ax, add_flyid_labels):
-    """
-    Helper function that actually plots the rasters.
-    """
-    from . import plot_helpers as plt_help
+        """
+        Helper function that actually plots the rasters.
+        """
+        from . import plot_helpers as plt_help
+        import pandas as pd
 
-    # Identify legitimate feeds; sort by time of first feed.
-    _feeding_flies = current_facet_feeds.sort_values(['RelativeTime_s','FeedDuration_s'])\
-                                        .FlyID.drop_duplicates()\
-                                        .tolist()
-    # Index the current faceted feeds by FlyID.
-    _current_facet_fly_index = current_facet_feeds.reset_index().set_index('FlyID')
+        # Identify legitimate feeds; sort by time of first feed.
+        _feeding_flies = current_facet_feeds.sort_values(['RelativeTime_s','FeedDuration_s'])\
+                                            .FlyID.drop_duplicates()\
+                                            .tolist()
+        # Index the current faceted feeds by FlyID.
+        _current_facet_fly_index = current_facet_feeds.reset_index().set_index('FlyID')
 
-    # Next, identify which flies did not feed (aka not in list above.)
-    _non_feeding_flies = current_facet_flies[current_facet_flies.AtLeastOneFeed == False].FlyID.tolist()
-    _flies_in_order = _feeding_flies + _non_feeding_flies
+        # Next, identify which flies did not feed (aka not in list above.)
+        _non_feeding_flies = current_facet_flies[current_facet_flies.AtLeastOneFeed == False].FlyID.tolist()
+        _flies_in_order = _feeding_flies + _non_feeding_flies
 
-    # Create the palette.
-    if color_by is not None:
-        colors = plt_help._make_categorial_palette(allfeeds, color_by)
-        palette = dict(zip(allfeeds[color_by].cat.categories, colors))
-
-    for k, fly in enumerate(_flies_in_order):
-        rasterplot_kwargs = dict(
-                ymin = (1/maxflycount) * (maxflycount-k-1),
-                ymax = (1/maxflycount) * (maxflycount-k),
-                alpha = 0.8)
-        try:
-            _current_facet_fly = _current_facet_fly_index.loc[fly]
-            if isinstance(_current_facet_fly, pd.Series):
-                rasterplot_kwargs['xmin'] = _current_facet_fly.RelativeTime_s
-                rasterplot_kwargs['xmax'] = _current_facet_fly.RelativeTime_s + \
-                                            _current_facet_fly.FeedDuration_s
-                rasterplot_kwargs['label'] = "_" * k + _current_facet_fly[color_by]
-                if color_by is not None:
-                    rasterplot_kwargs['color'] = palette[_current_facet_fly[color_by]]
-                plot_ax.axvspan(**rasterplot_kwargs)
-
-            elif isinstance(_current_facet_fly, pd.DataFrame):
-                start = _current_facet_fly.RelativeTime_s.tolist()
-                duration = _current_facet_fly.FeedDuration_s.tolist()
-
-                for j, feed_start in enumerate(start):
-                    current_color_cat = _current_facet_fly[color_by].iloc[j]
-                    rasterplot_kwargs['xmin'] = feed_start
-                    rasterplot_kwargs['xmax'] = feed_start + duration[j]
-                    rasterplot_kwargs['label'] = "_" * k + current_color_cat
+        for k, fly in enumerate(_flies_in_order):
+            rasterplot_kwargs = dict(
+                    ymin = (1/maxflycount) * (maxflycount-k-1),
+                    ymax = (1/maxflycount) * (maxflycount-k),
+                    alpha = 0.8)
+            try:
+                _current_facet_fly = _current_facet_fly_index.loc[fly]
+                if isinstance(_current_facet_fly, pd.Series):
+                    rasterplot_kwargs['xmin'] = _current_facet_fly.RelativeTime_s
+                    rasterplot_kwargs['xmax'] = _current_facet_fly.RelativeTime_s + \
+                                                _current_facet_fly.FeedDuration_s
                     if color_by is not None:
-                        rasterplot_kwargs['color'] = palette[current_color_cat]
+                        rasterplot_kwargs['color'] = palette[_current_facet_fly[color_by]]
                     plot_ax.axvspan(**rasterplot_kwargs)
 
-        except KeyError:
-            pass
+                elif isinstance(_current_facet_fly, pd.DataFrame):
+                    start = _current_facet_fly.RelativeTime_s.tolist()
+                    duration = _current_facet_fly.FeedDuration_s.tolist()
 
-        if add_flyid_labels:
-            if fly in _non_feeding_flies:
-                label_color = 'grey'
-            else:
-                label_color = 'black'
-            label = fly.split('_')[-1]
-            plot_ax.text(-85,
-                          (1/maxflycount)*(maxflycount-k-1) + (1/maxflycount)*.5,
-                           label,
-                           color=label_color,
-                           verticalalignment='center',
-                           horizontalalignment='right',
-                           fontsize=8)
+                    for j, feed_start in enumerate(start):
+                        rasterplot_kwargs['xmin'] = feed_start
+                        rasterplot_kwargs['xmax'] = feed_start + duration[j]
+                        if color_by is not None:
+                            current_color_cat = _current_facet_fly[color_by].iloc[j]
+                            rasterplot_kwargs['color'] = palette[current_color_cat]
+                        plot_ax.axvspan(**rasterplot_kwargs)
+
+            except KeyError:
+                pass
+
+            if add_flyid_labels:
+                if fly in _non_feeding_flies:
+                    label_color = 'grey'
+                else:
+                    label_color = 'black'
+                label = fly.split('_')[-1]
+                plot_ax.text(-85,
+                              (1/maxflycount)*(maxflycount-k-1) + (1/maxflycount)*.5,
+                               label,
+                               color=label_color,
+                               verticalalignment='center',
+                               horizontalalignment='right',
+                               fontsize=8)
 
     def rasters(self,
                 col=None,
@@ -124,8 +119,7 @@ class espresso_plotter():
                 add_flyid_labels=False,
                 fig_size=None,
                 ax=None,
-                gridlines_major=True,
-                gridlines_minor=True):
+                gridlines=True):
         """
         Produces a raster plot of feed events.
 
@@ -150,8 +144,9 @@ class espresso_plotter():
         ax: matplotlib Axes, default None
             Plot on specified matplotlib Axes.
 
-        gridlines_major, gridlines_minor: boolean, default True
-            Whether or not major and minor vertical gridlines are displayed.
+        gridlines: boolean, default True
+            Whether or not vertical gridlines are displayed at each major
+            (hourly) tick.
 
         Returns
         -------
@@ -160,6 +155,7 @@ class espresso_plotter():
         import numpy as np
         import matplotlib.pyplot as plt
         import matplotlib.patches as mpatches # for custom legends.
+        import pandas as pd
         import seaborn as sns
 
         from . import plot_helpers as plt_help
@@ -174,12 +170,12 @@ class espresso_plotter():
 
         if row is not None:
             # print("Plotting rows by {0}".format(row))
-            row_count = int(len(plotdf[row].cat.categories))
+            row_count = int(len(allfeeds[row].cat.categories))
         else:
             row_count = 1
         if col is not None:
             # print("Plotting columns by {0}".format(col))
-            col_count = int(len(plotdf[col].cat.categories))
+            col_count = int(len(allfeeds[col].cat.categories))
         else:
             col_count = 1
 
@@ -201,6 +197,12 @@ class espresso_plotter():
         facets_metadata = [a for a in facets if a in allflies.columns]
         faceted_flies = allflies.set_index(facets_metadata)
 
+        # Get the number of flies for each group, then identify which is
+        # the most numerous group. This is then used to scale the individual
+        # facets.
+        allflies_grpby = allflies.groupby(facets_metadata)
+        maxflycount = allflies_grpby.count().FlyID.max()
+
         # Get the total flycount.
         try:
             in_allflies = [a for a in facets if a in allflies.columns]
@@ -212,18 +214,10 @@ class espresso_plotter():
             # is simply the total number of flies.
             maxflycount = len(allflies)
 
-        # Create custom handles for the foodchoice.
-        # See http://matplotlib.org/users/legend_guide.html#using-proxy-artist
-        raster_legend_handles = []
-        for key in palette.keys():
-            patch = mpatches.Patch(color=palette[key], label=key)
-            raster_legend_handles.append(patch)
-
         # Initialise the figure.
-        # Initialise figure.
         sns.set(style='ticks',context='poster',font_scale=1.25)
         if fig_size is None:
-            x_inches = 10 * col_count
+            x_inches = 9 * col_count
             y_inches = 7 * row_count
         else:
             if isinstance(fig_size, tuple) or isinstance(fig_size, list):
@@ -241,6 +235,19 @@ class espresso_plotter():
         else:
             axx = ax
 
+        # Create the palette if so desired.
+        if color_by is not None:
+            colors = plt_help._make_categorial_palette(allfeeds, color_by)
+            palette = dict(zip(allfeeds[color_by].cat.categories, colors))
+            # Create custom handles for the foodchoice.
+            # See http://matplotlib.org/users/legend_guide.html#using-proxy-artist
+            raster_legend_handles = []
+            for key in palette.keys():
+                patch = mpatches.Patch(color=palette[key], label=key)
+                raster_legend_handles.append(patch)
+        else:
+            palette = None
+
         if row is not None and col is not None:
             for r, row_ in enumerate(faceted_feeds.index.get_level_values(row).unique()):
                 for c, col_ in enumerate(faceted_feeds.index.get_level_values(col).unique()):
@@ -255,8 +262,9 @@ class espresso_plotter():
                         # a Categorical index... so index step-by-step instead.
                         _temp_facet = faceted_feeds.loc[col_]
                         current_facet = _temp_facet[_temp_facet.index == row_]
-                    __plot_rasters(current_facet_feeds, current_facet_flies,
-                                   palette, plot_ax, add_flyid_labels)
+                    self.__plot_rasters(current_facet_feeds, current_facet_flies,
+                                        maxflycount, color_by, palette,
+                                        plot_ax, add_flyid_labels)
                     plot_ax.set_title("{}; {}".format(col_, row_))
 
         else:
@@ -266,11 +274,10 @@ class espresso_plotter():
                 print("Plotting {}".format(dim_))
                 plot_ax = axx[j] # the axes to plot on.
                 current_facet_feeds = faceted_feeds[faceted_feeds.index == dim_].dropna()
-                __plot_rasters(current_facet_feeds, current_facet_flies,
-                              palette, plot_ax, add_flyid_labels)
+                self.__plot_rasters(current_facet_feeds, current_facet_flies,
+                                    maxflycount, color_by, palette,
+                                    plot_ax, add_flyid_labels)
                 plot_ax.set_title(dim_)
-
-
 
 
         # Plot gridlines.
@@ -298,155 +305,14 @@ class espresso_plotter():
             sns.despine(ax=axx, **despine_kwargs)
             rasterlegend_ax = axx
 
-        rasterlegend_ax.legend(loc='upper left',
-                               bbox_to_anchor=(0,-0.15),
-                               handles=raster_legend_handles)
+        if color_by is not None:
+            rasterlegend_ax.legend(loc='upper left',
+                                   bbox_to_anchor=(0,-0.15),
+                                   handles=raster_legend_handles)
 
-
-
-        # # print("Coloring rasters by {0}".format(color_by))
-        # # print("Grouping rasters by {0}".format(group_by))
-        #
-        # # Get the total flycount.
-        # try:
-        #     group_by_cols = [a for a in [col, row] if a is not None]
-        #     check_grpby_col = allflies.groupby(group_by_cols)
-        #     maxflycount = check_grpby_col.count().FlyID.max()
-        # except KeyError:
-        #     # group_by is not a column in the metadata,
-        #     # so we assume that the number of flies in the raster plot
-        #     # is simply the total number of flies.
-        #     maxflycount = len(allflies)
-        #
-        # # Get the groups we will be grouping on, and coloring on.
-        # plot_rows = np.sort(allfeeds[group_by].unique())
-        # plot_cols = np.sort(allfeeds[color_by].unique())
-        # num_plots = int(len(groupby_grps))
-        #
-        # # Create the palette.
-        # colors = plt_help._make_categorial_palette(allfeeds,color_by)
-        # palette = dict(zip(color_grps, colors))
-        #
-        # # Create custom handles for the foodchoice.
-        # # See http://matplotlib.org/users/legend_guide.html#using-proxy-artist
-        # raster_legend_handles = []
-        # for key in palette.keys():
-        #     patch = mpatches.Patch(color=palette[key], label=key)
-        #     raster_legend_handles.append(patch)
-        #
-        # # Initialise the figure.
-        # # Initialise figure.
-        # sns.set(style='ticks',context='poster',font_scale=1.25)
-        # if fig_size is None:
-        #     x_inches = 10 * col_count
-        #     y_inches = 7 * row_count
-        # else:
-        #     if isinstance(fig_size, tuple) or isinstance(fig_size, list):
-        #         x_inches = fig_size[0]
-        #         y_inches = fig_size[1]
-        #     else:
-        #         raise TypeError('Please make sure figsize is a tuple of the '
-        #         'form (w,h) in inches.')
-        #
-        # if ax is None:
-        #     fig, axx = plt.subplots(nrows=row_count, ncols=col_count,
-        #                             figsize=(x_inches, y_inches),
-        #                             gridspec_kw={'wspace':0.3,
-        #                                          'hspace':0.3})
-        # else:
-        #     axx = ax
-        #
-        # # Loop through each panel.
-        # for c, grp in enumerate( groupby_grps ):
-        #     if len(groupby_grps)>1:
-        #         rasterax = axx[c]
-        #     else:
-        #         rasterax = axx
-        #     print('plotting {0} {1}'.format(grp,'rasters'))
-        #     print('Be patient, this can a while!')
-        #
-        #     # Plot the raster plots.
-        #     ## Plot vertical grid lines if desired.
-        #     grid_kwargs = dict(linestyle=':', alpha=0.5)
-        #     if gridlines_major:
-        #         rasterax.xaxis.grid(which='major',linewidth=0.25,**grid_kwargs)
-        #     if gridlines_minor:
-        #         rasterax.xaxis.grid(which='minor',linewidth=0.15,**grid_kwargs)
-        #
-        #     ## Grab only the flies we need.
-        #     tempfeeds = allfeeds[allfeeds[group_by] == grp].copy()
-        #     temp_allflies = tempfeeds.FlyID.unique().tolist()
-        #
-        #     ## Order the flies properly.
-        #     ### First, drop non-valid feeds, then sort by feed time and feed duration,
-        #     ### then pull out FlyIDs in that order.
-        #     temp_feeding_flies = tempfeeds[~np.isnan(tempfeeds['FeedDuration_s'])].\
-        #                             sort_values(['RelativeTime_s','FeedDuration_s']).FlyID.\
-        #                             drop_duplicates().tolist()
-        #     ### Next, identify which flies did not feed (aka not in list above.)
-        #     temp_non_feeding_flies = [fly for fly in temp_allflies
-        #     if fly not in temp_feeding_flies]
-        #     ### Then, join these two lists.
-        #     flies_in_order = temp_feeding_flies + temp_non_feeding_flies
-        #
-        #     ### Now, plot each fly as a row, and plot each feed as a colored raster for every fly.
-        #     flycount = int(len(flies_in_order))
-        #     for k, flyid in enumerate(flies_in_order):
-        #         tt = tempfeeds[tempfeeds.FlyID == flyid]
-        #         for idx in [idx for idx in tt.index if ~np.isnan(tt.loc[idx,'FeedDuration_s'])]:
-        #             rasterplot_kwargs = dict(xmin=tt.loc[idx,'RelativeTime_s'],
-        #                                    xmax=tt.loc[idx,'RelativeTime_s']+tt.loc[idx,'FeedDuration_s'],
-        #
-        #                                    ymin=(1/maxflycount)*(maxflycount-k-1),
-        #                                    ymax=(1/maxflycount)*(maxflycount-k),
-        #
-        #                                    color=palette[ tt.loc[idx,color_by] ],
-        #                                    label="_"*k + tt.loc[idx,color_by],
-        #
-        #                                    alpha=0.8)
-        #
-        #             rasterax.axvspan(**rasterplot_kwargs)
-        #
-        #         ### Plot the flyid labels if so desired.
-        #         if add_flyid_labels:
-        #             if flyid in temp_non_feeding_flies:
-        #                 label_color = 'grey'
-        #             else:
-        #                 label_color = 'black'
-        #             rasterax.text(-85, (1/maxflycount)*(maxflycount-k-1) + (1/maxflycount)*.5,
-        #                         flyid,
-        #                         color=label_color,
-        #                         verticalalignment='center',
-        #                         horizontalalignment='right',
-        #                         fontsize=8)
-        #     ## Aesthetic tweaks
-        #     rasterax.yaxis.set_visible(False)
-        #     rasterax.set_title(grp)
-        #
-        #     ### Format x-axis.
-        #     plt_help.format_timecourse_xaxis(rasterax, self.__expt_end_time)
-        #
-        # ## Despine accordingly (if multiple axes were produced.)
-        # ## Note the we remove the left spine (set to True).
-        # if len(groupby_grps) > 1:
-        #     for a in axx:
-        #         sns.despine(ax=a,left=True,trim=True,offset=5)
-        # else:
-        #     sns.despine(ax=axx,left=True,trim=True,offset=5)
-        #
-        # # Position the raster color legend.
-        # if num_plots>1:
-        #     rasterlegend_ax=axx
-        # else:
-        #     rasterlegend_ax=[ axx ]
-        #
-        # for a in rasterlegend_ax:
-        #     a.legend(loc='upper left',bbox_to_anchor=(0,-0.15),
-        #              handles=raster_legend_handles)
-        #
-        # # End and return the figure.
-        # if ax is None:
-        #     return axx
+        # End and return the figure.
+        if ax is None:
+            return axx
 
 
 
