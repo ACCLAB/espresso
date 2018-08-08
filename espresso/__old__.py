6ae82887ac396db06b44da300c3ef7196d02d443
Joses W. Ho <joseshowh@gmail.com>
2018-08-08 15:02:04 +0800

    archive old timecourse plotter

--- espresso/__old__.py
+++ espresso/__old__.py
@@ -183,3 +183,139 @@ def __cumulative_plotter(self, yvar, row, col, min_time, max_time,
       # End and return the figure.
       if ax is None:
           return axx
+
+
+
+def __timecourse_plotter(self,
+                         yvar,
+                         col,
+                         row,
+                         color_by,
+                         resample_by='5min',
+                         fig_size=None,
+                         gridlines=True,
+                         ax=None):
+
+    import numpy as np
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    from . import plot_helpers as plt_helper
+    from .._munger import munger as munge
+
+    feeds = self.__feeds.copy()
+
+    # Handle the group_by and color_by keywords.
+    munge.check_group_by_color_by(col, row, color_by, feeds)
+
+    ## DICTIONARY FOR MATCHING YVAR TO APPROPRIATE YLABEL.
+    yvar_ylabel_dict = {'AverageFeedVolumePerFly_µl':'Average Feed Volume Per Fly (µl)',
+                       'AverageFeedCountPerFly':'Average Feed Count Per Fly',
+                       'AverageFeedSpeedPerFly_µl/s':'Average Feed Speed Per Fly (µl/s)'}
+
+    ylab = yvar_ylabel_dict[yvar]
+
+    resamp_feeds = munge.groupby_resamp_sum(feeds, '5min')
+    resamp_feeds_sum = munge.sum_for_timecourse(resamp_feeds)
+    plotdf = self.__pivot_for_plot(resamp_feeds_sum, row, col, color_by)
+
+    # print("Coloring time course by {0}".format(color_by))
+    if row is not None:
+        # print("Plotting rows by {0}".format(row))
+        row_count = int(len(feeds[row].cat.categories))
+    else:
+        row_count = 1
+    if col is not None:
+        # print("Plotting columns by {0}".format(col))
+        col_count = int(len(feeds[col].cat.categories))
+    else:
+        col_count = 1
+
+
+    # DICTIONARY FOR MATCHING YVAR TO APPROPRIATE YLABEL.
+    yvar_ylabel_dict = {'AverageFeedVolumePerFly_µl':'Average Feed Volume Per Fly (µl)',
+                       'AverageFeedCountPerFly':'Average Feed Count Per Fly',
+                       'AverageFeedSpeedPerFly_µl/s':'Average Feed Speed Per Fly (µl/s)'}
+
+
+    # Initialise figure.
+    sns.set(style='ticks',context='poster')
+    if fig_size is None:
+        x_inches = 10 * col_count
+        y_inches = 7 * row_count
+    else:
+        if isinstance(fig_size, tuple) or isinstance(fig_size, list):
+            x_inches = fig_size[0]
+            y_inches = fig_size[1]
+        else:
+            raise TypeError('Please make sure figsize is a tuple of the '
+            'form (w,h) in inches.')
+
+    if ax is None:
+        fig,axx = plt.subplots(nrows=row_count, ncols=col_count,
+                               figsize=(x_inches,y_inches),
+                               gridspec_kw={'wspace':0.3,
+                                           'hspace':0.3})
+    else:
+        axx = ax
+
+    if row is not None and col is not None:
+        for r, row_ in enumerate(feeds[row].cat.categories):
+            for c, col_ in enumerate(feeds[col].cat.categories):
+                plot_ax = axx[r, c] # the axes to plot on.
+
+                # Reshape the current data to be plotted.
+                current_plot_df = plotdf.loc[(row_, col_), yvar]
+
+                # We unstack and transpose to create a 'long' dataset,
+                # where each column is a timecourse dataset to be plotted.
+                current_plot_df = current_plot_df.unstack().T
+
+                # Create the plot.
+                current_plot_df.plot.area(ax=plot_ax, stacked=True)
+                plot_ax.set_title("{}; {}".format(row_, col_))
+
+    else:
+        # We only have one dimension here.
+        plot_dim = [d for d in [row, col] if d is not None][0]
+        for j, dim_ in enumerate(feeds[plot_dim].cat.categories):
+
+            plot_ax = axx[j]
+
+            current_plot_df = plotdf.loc[dim_, yvar]
+            current_plot_df = current_plot_df.unstack().T
+
+            current_plot_df.plot.area(ax=plot_ax, stacked=True)
+            plot_ax.set_title(dim_)
+
+    # Normalize all the y-axis limits.
+    if row_count + col_count > 1:
+        plt_helper.normalize_ylims(axx.flatten(),
+                                 include_zero=True)
+        for plot_ax in axx.flatten():
+            # Format x-axis.
+            plt_helper.format_timecourse_xaxis(plot_ax, 21600)
+            # Set label for y-axis.
+            plot_ax.set_ylabel(ylab)
+            # Plot vertical grid lines if desired.
+            if gridlines:
+                plot_ax.xaxis.grid(True, which='major',
+                                   linestyle='dotted', #linewidth=1,
+                                   alpha=0.5)
+            # Despine and offset each axis.
+            sns.despine(ax=plot_ax, trim=True, offset=3)
+
+    for ax in axx.flatten()[:-1]:
+        ax.legend().set_visible(False)
+
+    if color_by is not None:
+        legend_title = ' '
+    else:
+        legend_title = color_by
+    axx.flatten()[-1].legend(loc='upper left',
+                             title=legend_title,
+                             bbox_to_anchor=(-0.05, -0.15))
+
+    # End and return the figure.
+    if ax is None:
+        return axx
