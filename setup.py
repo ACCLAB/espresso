2428ed18d43271c80201f0682c85218f6344e5a9
Joses W. Ho <joseshowh@gmail.com>
2018-08-27 15:08:41 +0800

    v0.4.2

--- setup.py
+++ setup.py
@@ -127,7 +127,7 @@ if __name__=="__main__":
         name='espresso',
         author='Joses Ho',
         author_email='joseshowh@gmail.com',
-        version='0.4.1',
+        version='0.4.2',
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
         packages=find_packages(),
